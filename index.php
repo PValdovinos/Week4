d6dbebd937665aff2e7efdcacc02e2c492703bc4
Pedro V.R. <ptreyes2@gmail.com>
2024-04-23 13:43:22 -0400

    Added lists and arrays to test

--- index.php
+++ index.php
@@ -19,6 +19,21 @@ $f3->route('GET /', function($f3){
 
     // Add data to the Fat-Free "hive"
     $f3->set('name', 'Pedro');
+    $f3->set('favGame', 'FFXV');
+    $f3->set('favFood', 'Chicken Katsu');
+    $f3->set('favPasstime', 'Taking drives');
+
+    // Define an array of fruits and add it to the hive
+    $fruits = array('apple', 'banana', 'orange');
+    $f3->set('fruitList', $fruits);
+
+    // Array of favorite games
+    $games = array('FFXV', 'Warhammer Darktide', 'Deeprock Galactic');
+    $f3->set('gameList', $games);
+
+    // Define an associative array of students
+    $students = array('101'=>'Mason', '102'=>'Tilak', '103'=>'Sage');
+    $f3->set('studentList', $students);
 
     //render a view page
     $view = new Template();
